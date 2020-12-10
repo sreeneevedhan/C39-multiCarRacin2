fa4cb0e432c2c50ccc2e2fbf7a9310b244639853
sreeneevedhan <sreeneevedhan@gmail.com>
2020-12-10 13:50:15 +0530

    reset

--- js/Game.js
+++ js/Game.js
@@ -70,7 +70,8 @@ class Game {
         cars[index-1].y = y;
 
         if (index === player.index){
-          cars[index - 1].shapeColor = "red";
+          fill("red");
+          ellipse(x,y,60,60);  
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y;
         }
